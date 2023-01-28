b0714c483c9327c124342e0d6db47d3b175241ec
not_matheusyy <matheusrodrigues.yy7@gmail.com>
2023-01-28 21:38:27 +0000

    update array, update #2

--- shop-carroussel/src/assets/data.img.ts
+++ shop-carroussel/src/assets/data.img.ts
@@ -1,14 +1,98 @@
-interface imagemData {
-  imagens: [string, string, string, string, string];
-  id: number;
+import { useState, useEffect } from "react";
+
+export const dataApiLocal = [
+  {
+    id: 466745,
+    name: "Geleia 700ml",
+    price: "12$",
+    url: "https://assets.pinterest.com/ext/embed.html?id=694328467569935470",
+    oldprice: "15$",
+    data: getData().data,
+  },
+  {
+    id: 4643546,
+    name: "Maionese",
+    price: "467.46$",
+    url: "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+    oldprice: "534.00$",
+    data: getData().data,
+  },
+  {
+    id: 35474,
+    name: "Lemon Curd",
+    price: "4540.00$",
+    url: "https://assets.pinterest.com/ext/embed.html?id=293085888257648843",
+    oldprice: "5034.00$",
+    data: getData().data,
+  },
+  {
+    id: 34668965,
+    name: "Italian",
+    price: "34.00$",
+    url: "https://assets.pinterest.com/ext/embed.html?id=1025694883857547870",
+    oldprice: "37.00$",
+    data: getData().data,
+  },
+  {
+    id: 3542474,
+    name: "Lemon Curd",
+    price: "4540.00$",
+    url: "https://assets.pinterest.com/ext/embed.html?id=293085888257648843",
+    oldprice: "5034.00$",
+    data: getData().data,
+  },
+];
+
+function getData() {
+  const time = new Date();
+  const [data, hours] = [time.getDate, time.getHours];
+  return { data: data, hours: hours };
 }
-export const imagemData: imagemData = {
-  imagens: [
-    "https://src.nappy.co/3030",
-    "https://src.nappy.co/3030",
-    "https://src.nappy.co/3030",
-    "https://src.nappy.co/3030",
-    "https://src.nappy.co/3030",
+
+const g = dataApiLocal.map((a) => {
+  const { data, id, name, oldprice, price, url } = a;
+  return;
+});
+
+export const datavalue = {
+  items: [
+    {
+      id: 4643546,
+      name: "Maionese",
+      price: "467.46$",
+      url: "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+      oldprice: "534.00$",
+      data: getData().data,
+    },
+    {
+      id: 4643546,
+      name: "Maionese",
+      price: "467.46$",
+      url: "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+      oldprice: "534.00$",
+      data: getData().data,
+    },
   ],
-  id: 34,
+  url: [
+    {
+      comidas: {
+        chocolate: {
+          bombom: ["a", "b", "v"],
+          ferreiro: "",
+        },
+        gelados: {
+          sorvetes: [],
+          bebidas: [],
+        },
+        vidros: {
+          produtos: [
+            "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+            "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+            "https://assets.pinterest.com/ext/embed.html?id=389279961551287362",
+          ],
+          ecomerce: [],
+        },
+      },
+    },
+  ],
 };
--- shop-carroussel/src/interface/carrossel.tsx
+++ shop-carroussel/src/interface/carrossel.tsx
@@ -1,34 +1,73 @@
 //styles
 import "./scss/carrossel.css";
 //imagens data
-import { imagemData } from "../assets/data.img";
+import { dataApiLocal, datavalue } from "../assets/data.img";
 //dependencias
-import { useEffect, useState } from "react";
+import React, { Children, useEffect, useState } from "react";
 
 export const Carrossel = () => {
-  const [image, setImage] = useState([{}]);
+  const [imagemState, setStateImage] = useState(dataApiLocal);
+  const [url, setUrl] = useState({ prev: 0, center: 0, next: 0 });
+  const [value, setValue] = useState({ prev: 1, center: 2, next: 3 });
+  const [vas, setVas] = useState({});
 
+  function next() {}
+  function last() {}
   useEffect(() => {
-    function getImage() {
-      return setImage(() => imagemData.imagens.map((a) => a));
-    }
-    getImage();
-  }, [imagemData]);
+    //esperado: img {http: nomes e url, id: id dos items}
+    //esperado: rec {data: data atual, hours: horario atual, location: local, recb: recebedor do item}
+    //criando um objeto com o valor inicial de useState e adicionando os items pegos
+    //de data.img.ts
+    setUrl({ center: value.center, next: value.next, prev: value.prev });
+  }, [value]);
 
   return (
     <>
-      {image.map((img, id) => {
-        return (
-          <div className="image-item-carrossel">
-            <img
-              src={`${img}`}
-              width="100px"
-              key={`${id++}0097`}
-              className={`image-carrossel${id}`}
-            />
-          </div>
-        );
-      })}
+      <div className="image-item-carrossel">
+        {datavalue.url.map((ind, num: number) => {
+          const { comidas } = ind;
+          comidas.vidros.produtos.map((ind, index) => {
+            const src = ind;
+            return (
+              <div className="content-frame">
+                <iframe
+                  src={src}
+                  scrolling="no"
+                  className="iframe-style"
+                ></iframe>
+                <p>name: nomes aqui</p>
+              </div>
+            );
+          });
+        })}
+        {imagemState.forEach((ind, index) => {
+          const { url } = ind;
+
+          console.log();
+          for (let i = 0; i < index.valueOf(); i++) {}
+        })}
+      </div>
+      <button
+        type="button"
+        onClick={() =>
+          setValue(({ center, next, prev }) => {
+            return { center: center + 1, next: next + 1, prev: prev + 1 };
+          })
+        }
+      >
+        next
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          setValue(({ center, next, prev }) => {
+            return { center: center - 1, next: next - 1, prev: prev - 1 };
+          })
+        }
+      >
+        return
+      </button>
+      <p>{[url.prev, url.center, url.next]}</p>
     </>
   );
 };
